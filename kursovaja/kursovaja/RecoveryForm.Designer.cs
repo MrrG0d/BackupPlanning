76cba6ea92ccaa0ec0a3736184d8e28f451825e0
MrrG0d <91164262+MrrG0d@users.noreply.github.com>
2023-06-30 18:59:16 +0300

    Add files via upload

---
+++ kursovaja/kursovaja/RecoveryForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace kursovaja
+{
+    partial class RecoveryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RecoveryForm));
+            this.DB_ComboBox = new System.Windows.Forms.ComboBox();
+            this.BD_Label = new System.Windows.Forms.Label();
+            this.OkButtonPath = new System.Windows.Forms.Button();
+            this.CatalogBackup_Label = new System.Windows.Forms.Label();
+            this.TextBoxPathBackup = new System.Windows.Forms.TextBox();
+            this.CatalogButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DB_ComboBox
+            // 
+            this.DB_ComboBox.FormattingEnabled = true;
+            this.DB_ComboBox.Location = new System.Drawing.Point(32, 52);
+            this.DB_ComboBox.Name = "DB_ComboBox";
+            this.DB_ComboBox.Size = new System.Drawing.Size(261, 24);
+            this.DB_ComboBox.TabIndex = 10;
+            // 
+            // BD_Label
+            // 
+            this.BD_Label.AutoSize = true;
+            this.BD_Label.Location = new System.Drawing.Point(29, 32);
+            this.BD_Label.Name = "BD_Label";
+            this.BD_Label.Size = new System.Drawing.Size(89, 17);
+            this.BD_Label.TabIndex = 11;
+            this.BD_Label.Text = "Выбрать БД";
+            // 
+            // OkButtonPath
+            // 
+            this.OkButtonPath.Location = new System.Drawing.Point(368, 200);
+            this.OkButtonPath.Name = "OkButtonPath";
+            this.OkButtonPath.Size = new System.Drawing.Size(95, 29);
+            this.OkButtonPath.TabIndex = 12;
+            this.OkButtonPath.Text = "Ok";
+            this.OkButtonPath.UseVisualStyleBackColor = true;
+            this.OkButtonPath.Click += new System.EventHandler(this.OkButtonPath_Click);
+            // 
+            // CatalogBackup_Label
+            // 
+            this.CatalogBackup_Label.AutoSize = true;
+            this.CatalogBackup_Label.Location = new System.Drawing.Point(29, 94);
+            this.CatalogBackup_Label.Name = "CatalogBackup_Label";
+            this.CatalogBackup_Label.Size = new System.Drawing.Size(147, 17);
+            this.CatalogBackup_Label.TabIndex = 13;
+            this.CatalogBackup_Label.Text = "Дирректория бэкапа";
+            // 
+            // TextBoxPathBackup
+            // 
+            this.TextBoxPathBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxPathBackup.Location = new System.Drawing.Point(32, 114);
+            this.TextBoxPathBackup.Name = "TextBoxPathBackup";
+            this.TextBoxPathBackup.Size = new System.Drawing.Size(321, 24);
+            this.TextBoxPathBackup.TabIndex = 14;
+            // 
+            // CatalogButton2
+            // 
+            this.CatalogButton2.Location = new System.Drawing.Point(373, 114);
+            this.CatalogButton2.Name = "CatalogButton2";
+            this.CatalogButton2.Size = new System.Drawing.Size(90, 24);
+            this.CatalogButton2.TabIndex = 15;
+            this.CatalogButton2.Text = "Выбрать...";
+            this.CatalogButton2.UseVisualStyleBackColor = true;
+            this.CatalogButton2.Click += new System.EventHandler(this.CatalogButton2_Click);
+            // 
+            // RecoveryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 241);
+            this.Controls.Add(this.CatalogButton2);
+            this.Controls.Add(this.TextBoxPathBackup);
+            this.Controls.Add(this.CatalogBackup_Label);
+            this.Controls.Add(this.OkButtonPath);
+            this.Controls.Add(this.BD_Label);
+            this.Controls.Add(this.DB_ComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RecoveryForm";
+            this.Text = "Бэкапер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox DB_ComboBox;
+        private System.Windows.Forms.Label BD_Label;
+        private System.Windows.Forms.Button OkButtonPath;
+        private System.Windows.Forms.Label CatalogBackup_Label;
+        private System.Windows.Forms.TextBox TextBoxPathBackup;
+        private System.Windows.Forms.Button CatalogButton2;
+    }
+}
---
+++ kursovaja/kursovaja/RecoveryForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32.TaskScheduler;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Diagnostics;
+
+namespace kursovaja
+{
+    public partial class RecoveryForm : Form
+    {
+        private string NameBD;
+        private string PathBackup;
+
+        public RecoveryForm()
+        {
+            InitializeComponent();
+            addingBD_ComboBox();
+        }
+
+        private void addingBD_ComboBox()
+        {
+            string connectString = "server=localhost;port=3306;username=root;password=mysql;";
+            MySqlConnection myConnection = new MySqlConnection(connectString);
+            string query = "show databases";
+            myConnection.Open();
+
+            MySqlCommand command = new MySqlCommand(query, myConnection);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                DB_ComboBox.Items.Add(reader.GetString(0));
+            }
+
+            reader.Close();
+            myConnection.Close();
+        }
+
+        private void OkButtonPath_Click(object sender, EventArgs e)
+        {
+            if (DB_ComboBox.Text == "")
+            {
+                MessageBox.Show("Не выбрана бд");
+                return;
+            }
+
+            if (TextBoxPathBackup.Text == "")
+            {
+                MessageBox.Show("Нет каталога бэкапа");
+                return;
+            }
+
+            this.PathBackup = TextBoxPathBackup.Text;
+            this.NameBD = DB_ComboBox.Text;
+
+            string commands = $@"D:
+                                 cd D:\Ampps\mysql\bin
+                                 mysqldump -uroot -pmysql --lock-all-tables {NameBD} < {PathBackup}.sql
+                                 mysqlcheck -u root -p --auto-repair {NameBD}";
+            
+            try
+            {
+                createProcess(commands);
+                MessageBox.Show("Восстановление завершено");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при восстановлении");
+            }
+
+            this.Close();
+        }
+
+        private void CatalogButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+
+            DialogResult result = openFileDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                TextBoxPathBackup.Text = openFileDlg.FileName;
+            }
+        }
+
+        void createProcess(string commands)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+            process.Start();
+
+            using (StreamWriter pWriter = process.StandardInput)
+            {
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    foreach (var line in commands.Split('\n'))
+                        pWriter.WriteLine(line);
+                }
+            }
+        }
+    }
+}
